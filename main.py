b5ffa76364da94fdc13368aaefa29ace4bbe3fbd
vasyalee <flightsho@gmail.com>
2022-11-27 22:56:58 +0300

    unload logic with fixed number of cranes and fixed unloading amount per hour

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import random
 def generateSchedule(_shipsNumber):
+    unloadTimes = (-100, -150, -250, -300)
     for i in range(_shipsNumber):
         schedule.append({"day": random.randint(1, 31), "hour": random.randint(0, 24), "minutes": random.randint(0, 60),
                          "ship": random.choice(ship_names), "cargoType": random.choice(cargo_types),
-                         "weight": random.randint(5000, 30000)})
+                         "weight": random.randint(5000, 30000), "unloadTime": random.choice(unloadTimes)})
 
 def acceptNewShip(_schedule):
     for i in range(len(_schedule)):
@@ -23,6 +24,30 @@ def startUnloading(_arrived):
             BULK_CRANE.append(_arrived[j])
             del _arrived[j]
 
+def unload(_CONT_CRANE, _LIQ_CRANE, _BULK_CRANE, _unloaded):
+    for _ship in _CONT_CRANE:
+        if _ship["weight"] > 0:
+            _ship["weight"] += _ship["unloadTime"]
+        elif _ship["weight"] <= 0:
+            _unloaded.append(_ship)
+            _CONT_CRANE.remove(_ship)
+
+    for _ship in _LIQ_CRANE:
+        if _ship["weight"] > 0:
+            _ship["weight"] += _ship["unloadTime"]
+        elif _ship["weight"] <= 0:
+            _unloaded.append(_ship)
+            _LIQ_CRANE.remove(_ship)
+
+    for _ship in _BULK_CRANE:
+        if _ship["weight"] > 0:
+            _ship["weight"] += _ship["unloadTime"]
+        elif _ship["weight"] <= 0:
+            _unloaded.append(_ship)
+            _BULK_CRANE.remove(_ship)
+
+
+
 
 ship_names = ["KSL-SANTOS", "VITAOCEAN", "SEA AMETHYST", "MSC BENEDETTA"
               "BLUE SEA", "ZENOVIA LADY", "MARLIN AMBER", "ALPHA UNITY",
@@ -35,6 +60,7 @@ cargo_types = ["CONT", "LIQ", "BULK"]
 
 schedule = []
 arrived = []
+unloaded = []
 
 
 
@@ -43,11 +69,12 @@ CONT_CRANE = []
 LIQ_CRANE = []
 BULK_CRANE = []
 
-generateSchedule(15)
+generateSchedule(3)
 
 
 for day in range(31):
     for hour in range(0, 24):
+        unload(CONT_CRANE, LIQ_CRANE, BULK_CRANE, unloaded)
         for minutes in range(0, 60):
             acceptNewShip(schedule)
         startUnloading(arrived)
@@ -76,4 +103,10 @@ for ship in arrived:
         print(f'{ship["day"]} DAY {ship["hour"]}:{ship["minutes"]} "{ship["ship"]}" {ship["cargoType"]} {ship["weight"]} tonne -> LIQ_CRANE')
     elif ship["cargoType"] == "BULK":
         print(f'{ship["day"]} DAY {ship["hour"]}:{ship["minutes"]} "{ship["ship"]}" {ship["cargoType"]} {ship["weight"]} tonne -> BULK_CRANE')
+print()
+print("Ships already unloaded: ")
+print()
+for ship in unloaded:
+    print(f'{ship["day"]} DAY {ship["hour"]}:{ship["minutes"]} "{ship["ship"]}" {ship["cargoType"]}')
+
 
